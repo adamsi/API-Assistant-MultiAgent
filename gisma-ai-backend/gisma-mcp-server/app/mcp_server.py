c90955d059655b23988a9e97664da7aa8a45862e
adam-sion <adamsion74@gmail.com>
2026-03-05 22:47:03 +0200

    feat: before excluding get_api_fruits

--- gisma-ai-backend/gisma-mcp-server/app/mcp_server.py
+++ gisma-ai-backend/gisma-mcp-server/app/mcp_server.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from fastmcp import FastMCP
@@ -8,15 +9,18 @@ from app.tools.generate_sql_graph import generate_sql_
 
 mcp = FastMCP("gisma-mcp-server")
 
+
 @mcp.tool
 def get_api_fruits(user_request: str):
     """get fruits from api by user_request filter"""
     return get_api_fruits_(user_request)
+
 
 @mcp.tool
 def generate_sql(user_request: str):
     """get data from db according to user_request"""
     return generate_sql_(user_request)
 
+
 if __name__ == "__main__":
-    mcp.run(transport="sse", port=8081)
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8081, path="/mcp")

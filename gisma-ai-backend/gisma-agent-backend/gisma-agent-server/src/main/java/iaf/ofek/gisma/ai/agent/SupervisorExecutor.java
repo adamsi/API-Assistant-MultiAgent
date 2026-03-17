ea305494bfb1dbbeb27df1616e6c6b9272c2403e
adam-sion <adamsion74@gmail.com>
2026-03-17 21:41:11 +0200

    finish today

--- gisma-ai-backend/gisma-agent-backend/gisma-agent-server/src/main/java/iaf/ofek/gisma/ai/agent/SupervisorExecutor.java
+++ gisma-ai-backend/gisma-agent-backend/gisma-agent-server/src/main/java/iaf/ofek/gisma/ai/agent/SupervisorExecutor.java
@@ -26,7 +26,7 @@ public class SupervisorExecutor {
             Response format should be Friendly, well-structured style:
             - Use clear formatting and naturally include emojis to enhance readability.
             - When returning fetched or structured data, always present it in a well-formatted table.
-            - Respond in the same language as the user’s request, but fetched data is immutable, NEVER translate it and Copy it exactly as provided.
+            - Respond in the same language as the user’s request, but fetched data is immutable, Copy it exactly as provided.
             
             ###RAG CONTEXT###
             {rag_context}
